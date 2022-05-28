6ce4475f83e10893b9c68d50ad20e358e39b2174
sylar <peilinok@163.com>
2022-05-29 02:06:58 +0800

    chore: fix package bug under mac

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import './index.css';
+import './app.global.scss';
 
 render(<App />, document.getElementById('root'));
